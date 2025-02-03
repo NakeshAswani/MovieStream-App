04e49f02b002241ac0b4038f6a3111489287432a
Nakesh <nakesh270704@gmail.com>
2025-02-03 16:56:37 +0530

    backend work

--- Backend/App/Routes/wishlistRoute.js
+++ Backend/App/Routes/wishlistRoute.js
@@ -1,21 +1,8 @@
 const express = require("express");
-// const multer = require("multer");
 const { addWishlist, getWishlist, deleteWishlist } = require("../Controllers/wishlistController");
 
 const wishlistRoute = express.Router();
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'uploads/wishlist')
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, new Date().getTime() + file.originalname);
-//     }
-// })
-
-// const upload = multer({ storage: storage });
-
-// wishlistRoute.post("/addWishlist", upload.single("Pimage"), addWishlist);
 wishlistRoute.post("/addWishlist", addWishlist);
 wishlistRoute.get("/getWishlist/:id", getWishlist);
 wishlistRoute.delete("/deleteWishlist/:id", deleteWishlist);
